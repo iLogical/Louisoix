bf088752b887bec8653813ea6536c70a18a8d900
iLogical <iLogical8502@gmail.com>
2021-07-17 20:43:57 +0100

    Initial Commit

---
+++ Louisoix.App/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using GameCore = Microsoft.Xna.Framework.Game;
+
+namespace Louisoix.App
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            using var game = new Game();
+            game.Run();
+        }
+    }
+
+    public class Game : GameCore
+    {
+        private readonly GraphicsDeviceManager _graphicsDeviceManager;
+        public Game()
+        {
+            _graphicsDeviceManager = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            base.Draw(gameTime);
+        }
+    }
+}
